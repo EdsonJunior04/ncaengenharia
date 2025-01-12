b74dfdb042a97bdde45c51cbedb45a6b322a475e
Lucas-Rubia <lucas.de.oliveira.rubia@gmail.com>
2025-01-12 13:53:45 -0300

    feat: Carousel made

---
+++ script.js
@@ -0,0 +1,43 @@
+const swiper = new Swiper('#slide-principal',{
+    // Optional parameters
+
+
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+
+    autoplay: {
+      delay: 1000,
+    },
+});
+
+
+
+const swiper1 = new Swiper("#slide-parceiros", {
+  spaceBetween: 0,
+  centeredSlides: true,
+  speed: 2500,
+  autoplay: {
+    delay: 1,
+  },
+  loop: true,
+  slidesPerView: 5 ,
+  allowTouchMove: false,
+  disableOnInteraction: true
+});
+
